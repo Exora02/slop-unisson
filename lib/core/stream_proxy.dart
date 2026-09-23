87a6513dfd1c66531cc3ba2f82498e0211ac4101
Exora02 <antoine.leyvraz@gmail.com>
2026-09-23 20:18:32 +0000

    Fix JWT decode: split payload segment + base64url->base64

--- lib/core/stream_proxy.dart
+++ lib/core/stream_proxy.dart
@@ -211,7 +211,10 @@ window.__deliverToken = function (t) {
   var w = window.__tokWaiters.splice(0);
   for (var i = 0; i < w.length; i++) w[i](t);
   try {
-    var payload = JSON.parse(atob(t));
+    var part = t.split('.')[1] || '';
+    part = part.replace(/-/g, '+').replace(/_/g, '/');
+    while (part.length % 4) part += '=';
+    var payload = JSON.parse(atob(part));
     window.spbridge && window.spbridge.postMessage(JSON.stringify(
         {type: 'token_scopes', scopes: payload.scope || 'NO-SCOPE-FIELD'}));
   } catch (e) {
